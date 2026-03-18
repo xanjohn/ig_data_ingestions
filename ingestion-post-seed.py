a84563bec60929c755bed6d200b0b3a38c025e60
xanjohn <mrrizkyn123@gmail.com>
2026-03-18 15:06:44 +0700

    Adding argv

--- ingestion-post-seed.py
+++ ingestion-post-seed.py
@@ -1,10 +1,18 @@
-from kafka import KafkaConsumer
 import json
-from conn import get_db_connection
+import argparse
+import sys
 from datetime import datetime
+from kafka import KafkaConsumer
+from conn import get_db_connection
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Kafka Ingestion Worker for Instagram Data')
+    parser.add_argument('--topic', type=str, default='ingestion-pipeline_instagram_mobile_raw', help='Nama Topic Kafka')
+    parser.add_argument('--groupid', type=str, default='ingestion-instagram-mobile', help='Group ID Consumer')
+    parser.add_argument('--batch', type=int, default=10, help='Jumlah data per sekali simpan ke DB')
+    return parser.parse_args()
 
-def convert_timestamp(ts):
+def format_ig_timestamp(ts):
     if ts is None or ts == 0 or ts == "":
         return None
     
@@ -17,94 +25,101 @@ def convert_timestamp(ts):
             ts_float = float(ts)
             
         return datetime.fromtimestamp(ts_float).strftime('%Y-%m-%d %H:%M:%S')
-    except Exception as e:
+    except Exception:
         return None
 
+def main():
+    args = get_args()
+    
+    conn = get_db_connection()
+    if not conn:
+        print("Can't connect database. Cek file conn.py")
+        return
+    cur = conn.cursor()
 
-conn = get_db_connection()
-cur = conn.cursor()
+    consumer = KafkaConsumer(
+        args.topic,
+        bootstrap_servers=[
+            '10.27.10.29:9092', 
+            '10.27.10.43:9092', 
+            '10.27.10.145:9092'
+        ],
+        auto_offset_reset='latest',
+        enable_auto_commit=False, 
+        group_id=args.groupid,
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
 
+    buffer_data = [] 
+    
+    print(f"--- Ingestion Worker Started ---")
+    print(f"Topic    : {args.topic}")
+    print(f"Group ID : {args.groupid}")
+    print(f"Batch    : {args.batch}")
+    print(f"----------------------")
 
-consumer = KafkaConsumer(
-    'ingestion-pipeline_instagram_mobile_raw',
-    bootstrap_servers=[
-        '10.27.10.29:9092', 
-        '10.27.10.43:9092', 
-        '10.27.10.145:9092'
-    ],
-    auto_offset_reset='latest',
-    enable_auto_commit=False,
-    group_id='ingestion-instagram-mobile',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-)
-
-buffer_data = [] 
-batch_size = 10 
-
-print(f"--- Waiting for data (Batch Size: {batch_size}) ---")
-
-try:
-    for message in consumer:
-        # print(message.value)
-        data = message.value
-        sections = data.get('raw', {}).get('media_grid', {}).get('sections', [])
-        
-        for section in sections:
-            layout_content = section.get('layout_content', {})
+    try:
+        for message in consumer:
+            data = message.value
+            sections = data.get('raw', {}).get('media_grid', {}).get('sections', [])
             
-            media_items = []
-            
-            if 'one_by_two_item' in layout_content:
-                clips = layout_content['one_by_two_item'].get('clips', {}).get('items', [])
-                media_items.extend(clips)
-            
-            if 'fill_items' in layout_content:
-                media_items.extend(layout_content['fill_items'])
+            for section in sections:
+                layout_content = section.get('layout_content', {})
+                media_items = []
                 
-            for item in media_items:
-                media = item.get('media', {})
-                if not media:
-                    continue
+                if 'one_by_two_item' in layout_content:
+                    clips = layout_content['one_by_two_item'].get('clips', {}).get('items', [])
+                    media_items.extend(clips)
                 
-                #Microsecond
-                device_timestamp = datetime.fromtimestamp(float(media.get('device_timestamp')) / 1000000).strftime('%Y-%m-%d %H:%M:%S')
-                
-                #Seconds
-                created_at = datetime.fromtimestamp(float(media.get('taken_at'))).strftime('%Y-%m-%d %H:%M:%S')
-                edited_at = datetime.fromtimestamp(float(media.get('edited_at'))) .strftime('%Y-%m-%d %H:%M:%S') if media.get('edited_at') else None
-                data_tuple = (
-                    str(media.get('pk')),
-                    'instagram',
-                    media.get('code'),
-                    format_ig_timestamp(media.get('device_timestamp')), 
-                    format_ig_timestamp(media.get('taken_at')),
-                    format_ig_timestamp(media.get('edited_at')),
-                    'pending',
-                    None,
-                )
-                buffer_data.append(data_tuple)
-                
-                print(f"Buffer: {len(buffer_data)}/{batch_size}", end='\r')
-                
-                if len(buffer_data) >= batch_size:
-                    query = """
-                        INSERT INTO post_link_seeds 
-                        (original_id, social_media, alternative_original_id, timestamp, created_at, updated_at, status, error) 
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                        ON DUPLICATE KEY UPDATE updated_at = VALUES(updated_at)
-                    """
-                    cur.executemany(query, buffer_data)
-                    conn.commit()
-                    consumer.commit()
-                    print(f"\n Success inserting {len(buffer_data)} data!")
-                    buffer_data = []
+                if 'fill_items' in layout_content:
+                    media_items.extend(layout_content['fill_items'])
+                    
+                for item in media_items:
+                    media = item.get('media', {})
+                    if not media:
+                        continue
+                    
 
-except KeyboardInterrupt:
-    print("\nStopping...")
-    if buffer_data:
-        cur.executemany(query, buffer_data)
-        conn.commit()
-        consumer.commit()
-finally:
-    cur.close()
-    conn.close()
+                    data_tuple = (
+                        str(media.get('pk')),             # original_id
+                        'instagram',                      # social_media
+                        media.get('code'),                # alternative_original_id (shortcode)
+                        format_ig_timestamp(media.get('device_timestamp')), 
+                        format_ig_timestamp(media.get('taken_at')),         # created_at
+                        None,                                            
+                        'pending',                        # status
+                        None                              # error
+                    )
+                    buffer_data.append(data_tuple)
+                    
+                    print(f"Buffer: {len(buffer_data)}/{args.batch}", end='\r')
+                    
+                    if len(buffer_data) >= args.batch:
+                        query = """
+                            INSERT INTO post_link_seeds 
+                            (original_id, social_media, alternative_original_id, timestamp, created_at, updated_at, status, error) 
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                            ON DUPLICATE KEY UPDATE updated_at = VALUES(updated_at)
+                        """
+                        cur.executemany(query, buffer_data)
+                        conn.commit()
+                        consumer.commit()
+                        
+                        print(f"\n[SUCCESS] Inserted {len(buffer_data)} data to DB.")
+                        buffer_data = []
+
+    except KeyboardInterrupt:
+        print("\nStopping worker...")
+    except Exception as e:
+        print(f"\n[CRITICAL ERROR] {e}")
+    finally:
+        if buffer_data:
+            cur.executemany(query, buffer_data)
+            conn.commit()
+            consumer.commit()
+        cur.close()
+        conn.close()
+        consumer.close()
+
+if __name__ == "__main__":
+    main()
